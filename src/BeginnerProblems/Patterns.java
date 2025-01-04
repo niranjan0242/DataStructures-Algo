2aac22e69de11b5e728636ddce760d104495829b
Niranjan_R <niri0242@gmail.com>
2025-01-04 18:31:10 +0530

    DataStructres&Algo

--- src/BeginnerProblems/Patterns.java
+++ src/BeginnerProblems/Patterns.java
@@ -2,9 +2,26 @@ package BeginnerProblems;
 
 public class Patterns {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void pattern1(int n) {
+        for (int i = 0;i<n;i++)
+        {
+            for(int j=0;j<=n-1;j++){
+                System.out.print("*");
+            }
+                System.out.println();
+        }
 
-	}
+        }
+
+    
+
+
+
+    public static void main(String[]args)
+    {
+       
+        int n=5;
+        pattern1(n);
+    }
 
 }
