8d0ab98a10e0ef47149501961bfddae624f1c03a
Niranjan_R <niri0242@gmail.com>
2025-01-10 21:12:29 +0530

    Sort an array of 0's 1's and 2's

---
+++ src/Arrays/SortAnAraray.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+public class SortAnAraray {
+	
+	    public void sortZeroOneTwo(int[] nums) {
+	        
+	        int n= nums.length;
+	         
+	       int low =0;
+	       int mid=0;
+	       int high=n-1;
+
+	      while(mid<=high){
+	        if(nums[mid]==0){
+	            int temp = nums[mid];
+	            nums[mid]=nums[low];
+	            nums[low]=temp;
+	            low++;
+	            mid++;
+
+	        }
+	        else if(nums[mid]==1){
+	            mid++;
+
+	        }
+	        else if(nums[mid]==2){
+	            int temp1 = nums[mid];
+	            nums[mid] = nums[high];
+	            nums[high]=temp1;
+	            high--;
+
+
+	        }
+	      }
+	      
+	    }
+
+
+	}
+
+
